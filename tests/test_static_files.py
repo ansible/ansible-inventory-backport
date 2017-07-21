7e26ba6f6b4a7f7aaa2afd0d14e04459f5ce6976
AlanCoding <arominge@redhat.com>
2017-07-21 14:55:20 -0400

    switch testing to example files from tempdir

--- tests/test_static_files.py
+++ tests/test_static_files.py
@@ -17,53 +17,7 @@ if path not in sys.path:
 from backport import InventoryCLI  # noqa
 
 
-TEST_INVENTORY_INI = '''\
-# Some comment about blah blah blah...
-
-[webservers]
-web1.example.com ansible_ssh_host=w1.example.net
-web2.example.com
-web3.example.com:1022
-
-[webservers:vars]   # Comment on a section
-webvar=blah
-
-[dbservers]
-db1.example.com
-db2.example.com
-
-[dbservers:vars]
-dbvar=ugh
-
-[servers:children]
-webservers
-dbservers
-
-[servers:vars]
-varb=B
-
-[all:vars]
-vara=A
-
-[others]
-10.11.12.13
-10.12.14.16:8022
-fe80::1610:9fff:fedd:654b
-[fe80::1610:9fff:fedd:b654]:1022
-::1
-'''
-
-
-@pytest.fixture(scope='session')
-def test_dir(tmpdir_factory):
-    return tmpdir_factory.mktemp('inv_files', numbered=False)
-
-
-@pytest.fixture(scope='session')
-def ini_file(test_dir):
-    fn = test_dir.join('test_hosts')
-    fn.write(TEST_INVENTORY_INI)
-    return fn
+EXAMPLE_FILE = os.path.join(TEST_DIR, 'data', 'legacy', 'inventory.ini')
 
 
 @pytest.fixture()
@@ -97,44 +51,39 @@ class TestLoadFile:
             output = json.loads(output)
         return output
 
-    def test_host_and_group_vars(self, ini_file, command):
-        instance = command(['-i', ini_file.dirname, '--list'])
-        instance.options.inventory = ini_file.dirname
+    def test_host_and_group_vars(self, command):
+        instance = command(['-i', EXAMPLE_FILE, '--list'])
+        instance.options.inventory = EXAMPLE_FILE
         output = self.get_output(instance)
         # Test vars set on hosts individually
         assert output['_meta']['hostvars']['web1.example.com']['ansible_ssh_host'] == 'w1.example.net'
         # Test vars set on a group
         assert output['dbservers']['vars']['dbvar'] == 'ugh'
 
-    def test_inventory_import_group_vars_file(self, ini_file, tmpdir_factory, command):
-        # Create an extra group_vars file for group webservers
-        f = tmpdir_factory.mktemp('inv_files/group_vars', numbered=False).join('webservers')
-        f.write('''webservers_only_variable: foobar\n''')
+    def test_inventory_import_group_vars_file(self, command):
+        # Test variables defined in group_vars directory
 
-        instance = command(['-i', ini_file.dirname, '--list'])
-        instance.options.inventory = ini_file.dirname
+        instance = command(['-i', EXAMPLE_FILE, '--list'])
+        instance.options.inventory = EXAMPLE_FILE
         output = self.get_output(instance)
 
         for host in ['web1.example.com', 'web2.example.com', 'web3.example.com']:
             assert output['_meta']['hostvars'][host]['webservers_only_variable'] == 'foobar'
 
-    def test_inventory_import_host_vars_file(self, ini_file, tmpdir_factory, command):
-        # Create an extra group_vars file for group webservers
-        f = tmpdir_factory.mktemp('inv_files/host_vars', numbered=False).join('db1.example.com')
-        f.write('''database_only: foobar\n''')
+    def test_inventory_import_host_vars_file(self, command):
+        # Test variables defined in host_vars directory
 
-        instance = command(['-i', ini_file.dirname, '--list'])
-        instance.options.inventory = ini_file.dirname
+        instance = command(['-i', EXAMPLE_FILE, '--list'])
+        instance.options.inventory = EXAMPLE_FILE
         output = self.get_output(instance)
 
         assert output['_meta']['hostvars']['db1.example.com']['database_only'] == 'foobar'
 
-    def test_ansible_facts_not_in_hostvars(self, test_dir, command):
-        fn = test_dir.join('test_hosts')
-        fn.write('''foo.bar.com\n''')
+    def test_ansible_facts_not_in_hostvars(self, command):
+        fn = os.path.join(TEST_DIR, 'data', 'single_host', 'single_host')
 
-        instance = command(['-i', fn.dirname, '--list'])
-        instance.options.inventory = fn.dirname
+        instance = command(['-i', fn, '--list'])
+        instance.options.inventory = fn
         output = self.get_output(instance)
 
         assert 'ansible_facts' not in output['_meta']['hostvars']['foo.bar.com']
